032bf55c8f927201fac65e5527fa77cd63d75395
mnayan9985 <139874289+mnayan9985@users.noreply.github.com>
2023-07-18 18:18:22 +0530

    Update streamlit_app.py
    adding imojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 #created the main python file
 import streamlit
-streamlit.title('my parents new healthy dinner')
-streamlit.header('breakfast menu')
-streamlit.text('oats, milk, pomogranet')
-streamlit.text('fish, fruits')
+streamlit.title('My Parents new Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣omega 3, oats, milk, pomogranet')
+streamlit.text('🥗spinach, fish, fruits')
+streamlit.text('🐔Boild egg, Chicken')
+streamlit.text('🥑🍞avacado toasting')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
