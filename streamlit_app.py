215baa532fc5ee032c6a0ae86e49a08b2fa2e818
mnayan9985 <139874289+mnayan9985@users.noreply.github.com>
2023-07-18 18:12:51 +0530

    Update streamlit_app.py
    added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 #created the main python file
 import streamlit
 streamlit.title('my parents new healthy dinner')
+streamlit.header('breakfast menu')
+streamlit.text('oats, milk, pomogranet')
+streamlit.text('fish, fruits')
